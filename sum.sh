0265aa0230d3d136b2158ee59e456c741fab8898
Sasikanth Pamuru <pamuru.sasi@gmail.com>
2018-11-30 16:35:19 +0530

    from local

--- sum.sh
+++ sum.sh
@@ -2,4 +2,4 @@ echo "This is a script for adding two numbers"
 NUMBER=$(($ONE + $TWO))
 echo $NUMBER
 echo "Addition of two number is completed"
-echo "Done on workspace"
+echo "Done on workspace now"
